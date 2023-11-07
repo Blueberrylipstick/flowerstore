2f4d923162d8dab68faddb9bdc44914d2769e752
Blueberrylipstick <juliasavchyn1@gmail.com>
2023-11-07 16:57:31 +0200

    something new

--- src/main/java/ua/edu/ucu/apps/flowerstore/flowers/Flower.java
+++ src/main/java/ua/edu/ucu/apps/flowerstore/flowers/Flower.java
@@ -1,5 +1,9 @@
 package ua.edu.ucu.apps.flowerstore.flowers;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,19 +11,22 @@ import lombok.Setter;
 import lombok.ToString;
 
 @Setter @Getter @AllArgsConstructor @ToString @NoArgsConstructor
+@Entity
 public class Flower {
+    @Id
+    private int id;
     private double sepalLength;
     private FlowerColor color;
     private double price;
 
-    public Flower(Flower flower) {
-        this.color = flower.color;
-        this.sepalLength = flower.sepalLength;
-        this.price = flower.price;
-    }
+    // public Flower(Flower flower) {
+    //     this.color = flower.color;
+    //     this.sepalLength = flower.sepalLength;
+    //     this.price = flower.price;
+    // }
 
-    public String getColor() {
-        return color.toString();
-    }
+    // public String getColor() {
+    //     return color.toString();
+    // }
 }
 
--- src/main/java/ua/edu/ucu/apps/flowerstore/order/FlowerPack.java
+++ src/main/java/ua/edu/ucu/apps/flowerstore/order/FlowerPack.java
@@ -1,17 +1,18 @@
 package ua.edu.ucu.apps.flowerstore.order;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import ua.edu.ucu.apps.flowerstore.flowers.Flower;
 
-@Getter
+@Getter @AllArgsConstructor
 public class FlowerPack {
     private Flower flower;
     private int quantity;
 
-    public FlowerPack(Flower flower, int quantity) {
-        this.flower = new Flower(flower);
-        this.quantity = quantity;
-    }
+    // public FlowerPack(Flower flower, int quantity) {
+    //     this.flower = new Flower(flower);
+    //     this.quantity = quantity;
+    // }
 
     public double getPrice() {
         return this.flower.getPrice() * this.quantity;
